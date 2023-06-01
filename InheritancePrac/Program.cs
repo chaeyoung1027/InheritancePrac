406fc6afb54f0746557af13d2a4c0e8d9850b98a
Chaeyoung Lim <rnldyal1027@naver.com>
2023-06-01 09:46:53 +0900

    다형성을 활용하기 위한 is와 as 키워드

--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -5,6 +5,7 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace InheritancePrac32
 {
     internal class Program
@@ -18,18 +19,29 @@ namespace InheritancePrac32
 
             List<Animal> Animals = new List<Animal>() { new Dog(), new Dog(), new Dog(),
                 new Cat(), new Cat(), new Cat() };
+
             foreach (var item in Animals)
             {
                 item.Eat();
                 item.Sleep();
-                item.Bark();
+
+                var _dog = item as Dog;
+                if (_dog != null) { _dog.Bark(); }
+
+                var _cat = item as Cat;
+                if (_cat != null) { _cat.Meow(); }
+
+                if (item is Dog)
+                {
+                    ((Dog)item).Bark();
+                }
+                else if (item is Cat)
+                {
+                    ((Cat)item).Meow();
+                }
+
             }
-            foreach (var item in Cats)
-            {
-                item.Eat();
-                item.Sleep();
-                item.Meow();
-            }
+
         }
     }
 }
